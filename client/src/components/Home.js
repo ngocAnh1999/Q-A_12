724f7dde81bf2dea064bdf7ce2b471a3e3c86eac
NgocAnh <anhshmily2512@gmail.com>
2019-10-04 17:39:34 +0700

    study react-redux

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<div>
+            <h2>Hello World!!!</h2>
+        </div>);
+    }
+}
+export default Home;
